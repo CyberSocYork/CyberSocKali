90bcf77fc55f76a15ebf017ae646c3bef9138446
Jacob Turner <jacob11turner@gmail.com>
2020-10-22 09:42:41 +0100

    Fix grub boot issue

--- config/includes.chroot/etc/skel/.config/plasma-locale-settings.sh
+++
@@ -1,5 +0,0 @@
-# Generated script, do not edit
-# Exports language-format specific env vars from startkde.
-# This script has been generated from kcmshell5 formats.
-# It will automatically be overwritten from there.
-export LANG=en_GB.UTF-8
